83aa6e19252d93cc5dde0bc156bee867dbe17d5b
Adil Temirgaliyev <temirgaliyevadil@gmail.com>
2023-11-15 14:29:33 +0500

    typed the statistics component

--- src/screens/Result/Statistic.tsx
+++ src/screens/Result/Statistic.tsx
@@ -3,12 +3,17 @@ import {Dimensions, Image, StyleSheet, Text, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
 const width = Dimensions.get('screen').width;
+interface IStaticItem {
+  title: string;
+  firstParam: number;
+  secondParam: number;
+}
 
-const StatisticItems = [
-  {title: 'Lose Weight', firstParam: '33%', secontParam: '67%'},
-  {title: 'Height Increase', firstParam: '88%', secontParam: '12%'},
-  {title: 'Muscle Mass Increase', firstParam: '57%', secontParam: '43%'},
-  {title: 'Abs', firstParam: '89%', secontParam: '11%'},
+const StatisticItems: IStaticItem[] = [
+  {title: 'Lose Weight', firstParam: 33, secondParam: 67},
+  {title: 'Height Increase', firstParam: 88, secondParam: 12},
+  {title: 'Muscle Mass Increase', firstParam: 57, secondParam: 43},
+  {title: 'Abs', firstParam: 89, secondParam: 11},
 ];
 
 export const Statistic = () => {
@@ -25,20 +30,20 @@ export const Statistic = () => {
         <View key={i} style={styles.main}>
           <Text style={styles.mainTitle}>{e.title}</Text>
           <View style={styles.mainInner}>
-            <Text style={styles.mainInnerText}>{e.firstParam}</Text>
+            <Text style={styles.mainInnerText}>{e.firstParam}%</Text>
             <LinearGradient
               colors={['#EEA4CE', '#C58BF2']}
               start={{x: 1, y: 0}}
               end={{x: 1, y: 0}}
-              style={[styles.firstParam, {width: e.firstParam}]}
+              style={[styles.firstParam, {width: `${e.firstParam}%`}]}
             />
             <LinearGradient
               colors={['#92A3FD', '#9DCEFF']}
               start={{x: 0.5, y: 1}}
               end={{x: 0, y: 1}}
-              style={[styles.secondParam, {width: e.secontParam}]}
+              style={[styles.secondParam, {width: `${e.secondParam}%`}]}
             />
-            <Text style={styles.mainInnerText}>{e.secontParam}</Text>
+            <Text style={styles.mainInnerText}>{e.secondParam}%</Text>
           </View>
         </View>
       ))}
