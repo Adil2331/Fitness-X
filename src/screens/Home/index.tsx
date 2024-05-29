7fc920250bf7adf77cf50bf68353475a8d64a0af
Adil Temirgaliyev <temirgaliyevadil@gmail.com>
2024-05-29 16:12:49 +0500

    first test commit

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -21,6 +21,7 @@ import {HomeLatestActivity} from '../../components/HomeLatestActivity';
 
 export const Home: React.FC = () => {
   const {navigate} = useNavigation<PrivateStackScreenProps['navigation']>();
+  console.log('test');
   return (
     <SafeAreaLayout style={styles.container} bottom top>
       <View style={styles.headerContainer}>
